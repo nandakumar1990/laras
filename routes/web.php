ecf913f13302b07bfd3a4a3c20da9ce280b60a5a
unknown <nandakumar1990>
2020-09-04 14:43:15 +0530

    my test work

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home2', 'HomeController@index')->name('home');
+Route::get('/home3', 'HomeController@index')->name('home');
