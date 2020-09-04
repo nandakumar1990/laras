ce0c1e05688eea8d76ba930bdfb680f0e14e9cbd
unknown <nandakumar1990>
2020-09-04 14:53:35 +0530

    push in dev branch

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/home2', 'HomeController@index')->name('home');
-Route::get('/home3', 'HomeController@index')->name('home');
+
